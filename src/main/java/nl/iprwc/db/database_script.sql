05f627768e37ea82b41caa5944eea0aee3bf5ab1
Yme Brugts <yme@resolutionsbv.nl>
2019-02-05 15:09:38 +0100

    changed db script

--- src/main/java/nl/iprwc/db/database_script.sql
+++ src/main/java/nl/iprwc/db/database_script.sql
@@ -3,24 +3,24 @@ CREATE TABLE item (
 	name VARCHAR(100) NOT NULL,
 	description VARCHAR(500) NOT NULL,
 	price_cents INTEGER NOT NULL
-	);
+);
 	
 CREATE TABLE account (
 	email VARCHAR(100) PRIMARY KEY,
 	password VARCHAR(32) NOT NULL,
 	is_admin INTEGER NOT NULL
-	);
+);
 	
 CREATE TABLE location (
-	account_email VARCHAR(100) REFERENCES account(email) ON DELETE CASCADE,
+	account_email VARCHAR(100) REFERENCES account(email) ON DELETE CASCADE NOT NULL,
 	street_name VARCHAR(100) NOT NULL,
 	house_nr VARCHAR(10) NOT NULL,
 	zipcode CHAR(10) NOT NULL,
-	city VARCHAR(50)
-	);
+	city VARCHAR(50) NOT NULL
+);
 	
-CREATE TABLE basket (
-	account_email VARCHAR(100) REFERENCES account (email) ON DELETE CASCADE,
-	item_id INTEGER UNIQUE NOT NULL REFERENCES item (id) ON DELETE CASCADE
-	);
-	
+CREATE TABLE orders (
+  account_email VARCHAR 100 REFERENCES account(email) ON DELETE RESTRICT NOT NULL,
+  product_id INTEGER REFERENCES item(id) NOT NULL,
+  amount INTEGER NOT NULL
+);
